76c7720ccd1c77bdc17c63d26f0de684564cee20
masutcu <masutcu@gmail.com>
2023-05-26 19:03:12 +0300

    odev

--- src/test/java/odevler/Odev2.java
+++ src/test/java/odevler/Odev2.java
@@ -1,6 +1,18 @@
 package odevler;
 
-public class Odev2 {
+import base_urls.ReqresBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class Odev2 extends ReqresBaseUrl {
 
  /*
  task 2
@@ -17,5 +29,26 @@ public class Odev2 {
         And
             Response body should be empty
      */
+ @Test
+ public void odev02() {
+     //set th url
+     spec.pathParam("first","23");
 
+     //Send the request and get the response
+     Response response=given(spec).get("{first}"); //accept(ContentType.JSON) BaseUrl clasında ekli zaten
+     response.prettyPrint();
+
+     //Do Assertion
+
+     response.then().
+             statusCode(404).
+             contentType(ContentType.JSON).
+             statusLine("HTTP/1.1 404 Not Found");
+     //Server is "cloudflare"
+     String server=response.header("Server");
+     Assert.assertEquals("cloudflare",server);
+     //Response body should be empty
+     assertFalse(response.asString().contains("id"));
+
+ }
 }
--- src/test/java/odevler/Odev3.java
+++ src/test/java/odevler/Odev3.java
@@ -1,6 +1,18 @@
 package odevler;
 
-public class Odev3 {
+import base_urls.ReqresBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+public class Odev3 extends ReqresBaseUrl {
       /*
     task3
        Given
@@ -19,5 +31,55 @@ public class Odev3 {
            “last_name” is "Weaver"
        And
            "text" is "To keep ReqRes free, contributions towards server costs are appreciated!"
+
+           {
+    "data": {
+        "id": 2,
+        "email": "janet.weaver@reqres.in",
+        "first_name": "Janet",
+        "last_name": "Weaver",
+        "avatar": "https://reqres.in/img/faces/2-image.jpg"
+    },
+    "support": {
+        "url": "https://reqres.in/#support-heading",
+        "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
+    }
     */
+      @Test
+      public void odev03() {
+          //set th url
+          spec.pathParams("first","2");
+
+          //Send the request and get the response
+          Response response=given(spec).get("{first}"); //accept(ContentType.JSON) BaseUrl clasında ekli zaten
+          response.prettyPrint();
+
+          String body=response.asString();
+          System.out.println(body);
+
+          //Do Assertion
+          response.then().
+                  statusCode(200).
+                  contentType(ContentType.JSON);
+          response.then().
+                  body("data.email",equalTo("janet.weaver@reqres.in"),
+                          "data.first_name",equalTo("Janet"),
+                          "data.last_name",equalTo("Weaver"),
+                          "support.text",equalTo("To keep ReqRes free, contributions towards server costs are appreciated!"));
+
+
+
+
+
+        //diğer yol hard
+        /*    Assert.assertTrue(body.contains("Janet"));
+          Assert.assertTrue(body.contains("Weaver"));
+          Assert.assertTrue(body.contains("janet.weaver@reqres.in"));
+          Assert.assertTrue(body.contains("To keep ReqRes free, contributions towards server costs are appreciated!"));
+
+
+   */
+
+
+      }
 }
